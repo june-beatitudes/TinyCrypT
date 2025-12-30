86a70abaab23e8c538224fe046d26fc1d5cb6e8f
Juniper Beatitudes <juniper@eyes-like-fire.org>
2025-12-30 11:42:25 -0600

    Dev environment setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "TinyCrypT Build Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      shl = pkgs.mkShell {
+        name = "tct-tooling-env";
+        packages = with pkgs; [
+          cmake
+          gcc
+          clang-tools
+        ];
+      };
+    in
+    {
+      devShells.${system}.default = shl;
+    };
+}
