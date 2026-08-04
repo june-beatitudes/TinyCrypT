b521f6be2d25fcaf7b87139954b2716ef1f971eb
Juniper Beatitudes <juniper@eyes-like-fire.org>
2026-08-04 00:54:38 -0500

    Workaround for BINSEC bug on big-endian architectures

--- proofs/sha2/harness.c
+++ proofs/sha2/harness.c
@@ -6,11 +6,17 @@
 #define MAX_SIZE 65536
 uint8_t to_hash[MAX_SIZE];
 uint8_t sha256[32];
-size_t n = MAX_SIZE;
+uint32_t n = MAX_SIZE;
 
 int
 main (int argc, const char **argv)
 {
-  tct_sha256 (to_hash, n, sha256);
+#ifndef TCT_LITTLE_ENDIAN
+  // Deal with a bug in BINSEC where globals are mapped as little-endian even
+  // when simulating big-endian architectures
+  n = ((n >> 24) & 0x000000FF) | ((n >> 8) & 0x0000FF00)
+      | ((n << 8) & 0x00FF0000) | ((n << 24) & 0xFF000000);
+#endif
+  tct_sha256 (to_hash, (uint64_t)n, sha256);
   exit (sha256[0]);
 }
