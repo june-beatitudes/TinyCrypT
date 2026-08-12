8bb3597c0c0e3d7a558885c7e25358a6e1f2cebb
Juniper Beatitudes <juniper@eyes-like-fire.org>
2026-08-12 09:02:13 -0500

    Made CBMC contracts less hard to look at

--- tinycrypt/internal/cbmc.h
+++ tinycrypt/internal/cbmc.h
@@ -2,11 +2,27 @@
 #define TCT_INTERNAL_CBMC_H
 
 #ifdef CBMC
+
 #define __func_contract__(x) x
 #define __loop_contract__(x) x
+#define requires(x) __CPROVER_requires (x)
+#define ensures(x) __CPROVER_ensures (x)
+
+#define is_fresh(x, n) __CPROVER_is_fresh (x, n)
+#define memory_slice(x, n) __CPROVER_object_upto (x, n)
+#define entire_object(x) __CPROVER_object_whole (x)
+#define is_writable(x, n) __CPROVER_w_ok (x, n)
+#define is_readable(x, n) __CPROVER_r_ok (x, n)
+
+#define loop_invariant(x) __CPROVER_loop_invariant (x)
+#define loop_decreases(x) __CPROVER_decreases (x)
+#define assigns(x) __CPROVER_assigns (x)
+
 #else
+
 #define __func_contract__(x)
 #define __loop_contract__(x)
+
 #endif
 
 #endif
--- tinycrypt/portable/sha3.c
+++ tinycrypt/portable/sha3.c
@@ -5,20 +5,24 @@
 #include "tinycrypt/sha3.h"
 
 static uint64_t
-rotl64 (uint64_t x, uint32_t c)
-    __func_contract__ (__CPROVER_requires (0 < c % 64))
+rotl64 (uint64_t x, uint32_t c) __func_contract__ (
+  requires (0 < c % 64)
+)
 {
   return (x << (c % 64)) | (x >> (64 - (c % 64)));
 }
 
 static uint64_t
-from_le64 (const uint8_t *x)
-    __func_contract__ (__CPROVER_requires (__CPROVER_is_fresh (x, 8)))
+from_le64 (const uint8_t *x) __func_contract__ (
+  requires (is_fresh (x, 8))
+)
 {
   uint64_t u = 0x0;
   for (unsigned int i = 0; i < 8; ++i)
-    __loop_contract__ (__CPROVER_loop_invariant (i <= 8)
-                           __CPROVER_decreases (8 - i))
+    __loop_contract__ (
+      loop_invariant (i <= 8)
+      loop_decreases (8 - i)
+    )
     {
       u <<= 8;
       u |= x[7 - i];
@@ -28,13 +32,17 @@ from_le64 (const uint8_t *x)
 
 static void
 to_le64 (uint64_t u, uint8_t *x)
-    __func_contract__ (__CPROVER_requires (__CPROVER_w_ok (x, 8))
-                           __CPROVER_assigns (__CPROVER_object_upto (x, 8)))
+    __func_contract__ (
+      requires (is_writable (x, 8))
+      assigns (memory_slice (x, 8))
+    )
 {
   for (unsigned int i = 0; i < 8; ++i)
-    __loop_contract__ (__CPROVER_assigns (__CPROVER_object_upto (x, 8), i, u)
-                           __CPROVER_loop_invariant (i <= 8)
-                               __CPROVER_decreases (8 - i))
+    __loop_contract__ (
+      assigns (memory_slice (x, 8); i; u)
+      loop_invariant (i <= 8)
+      loop_decreases (8 - i)
+    )
     {
       x[i] = u & 0xFF;
       u >>= 8;
@@ -52,32 +60,42 @@ static const uint64_t RC[24]
         0x8000000000008080, 0x0000000080000001, 0x8000000080008008 };
 
 static void
-kp (uint8_t state[TCT_KP1600_STATE_LEN], uint8_t rounds) __func_contract__ (
-    __CPROVER_assigns (__CPROVER_object_upto (state, TCT_KP1600_STATE_LEN))
-        __CPROVER_requires (__CPROVER_is_fresh (state, TCT_KP1600_STATE_LEN))
-            __CPROVER_requires (rounds <= 24))
+kp (uint8_t state[TCT_KP1600_STATE_LEN], uint8_t rounds)
+    __func_contract__ (
+      assigns (memory_slice (state, TCT_KP1600_STATE_LEN))
+      requires (is_fresh (state, TCT_KP1600_STATE_LEN))
+      requires (rounds <= 24)
+    )
 {
   uint64_t lanes[5][5];
   for (unsigned int x = 0; x < 5; ++x)
-    __loop_contract__ (__CPROVER_loop_invariant (x <= 5)
-                           __CPROVER_decreases (5 - x))
+    __loop_contract__ (
+      loop_invariant (x <= 5)
+      loop_decreases (5 - x)
+    )
     {
       for (unsigned int y = 0; y < 5; ++y)
-        __loop_contract__ (__CPROVER_loop_invariant (y <= 5)
-                               __CPROVER_decreases (5 - y))
+        __loop_contract__ (
+          loop_invariant (y <= 5)
+          loop_decreases (5 - y)
+        )
         {
           lanes[x][y] = from_le64 (&state[8 * (x + 5 * y)]);
         }
     }
   for (unsigned int round = 0; round < rounds; ++round)
-    __loop_contract__ (__CPROVER_loop_invariant (round <= rounds)
-                           __CPROVER_decreases (rounds - round))
+    __loop_contract__ (
+      loop_invariant (round <= rounds)
+      loop_decreases (rounds - round)
+    )
     {
       uint64_t C[5];
       uint64_t D[5];
       for (unsigned int x = 0; x < 5; ++x)
-        __loop_contract__ (__CPROVER_loop_invariant (x <= 5)
-                               __CPROVER_decreases (5 - x))
+        __loop_contract__ (
+          loop_invariant (x <= 5)
+          loop_decreases (5 - x)
+        )
         {
           C[x] = lanes[x][0];
           C[x] ^= lanes[x][1];
@@ -86,18 +104,24 @@ kp (uint8_t state[TCT_KP1600_STATE_LEN], uint8_t rounds) __func_contract__ (
           C[x] ^= lanes[x][4];
         }
       for (unsigned int x = 0; x < 5; ++x)
-        __loop_contract__ (__CPROVER_loop_invariant (x <= 5)
-                               __CPROVER_decreases (5 - x))
+        __loop_contract__ (
+          loop_invariant (x <= 5)
+          loop_decreases (5 - x)
+        )
         {
           D[x] = C[(x + 4) % 5] ^ rotl64 (C[(x + 1) % 5], 1);
         }
       for (unsigned int y = 0; y < 5; ++y)
-        __loop_contract__ (__CPROVER_loop_invariant (y <= 5)
-                               __CPROVER_decreases (5 - y))
+        __loop_contract__ (
+          loop_invariant (y <= 5)
+          loop_decreases (5 - y)
+        )
         {
           for (unsigned int x = 0; x < 5; ++x)
-            __loop_contract__ (__CPROVER_loop_invariant (x <= 5)
-                                   __CPROVER_decreases (5 - x))
+            __loop_contract__ (
+              loop_invariant (x <= 5)
+              loop_decreases (5 - x)
+            )
             {
               lanes[x][y] ^= D[x];
             }
@@ -106,12 +130,13 @@ kp (uint8_t state[TCT_KP1600_STATE_LEN], uint8_t rounds) __func_contract__ (
       uint64_t y = 0;
       uint64_t current = lanes[x][y];
       for (unsigned int t = 0; t < 24; ++t)
-        __loop_contract__ (__CPROVER_assigns (t, x, y, current,
-                                              __CPROVER_object_whole (lanes))
-                               __CPROVER_loop_invariant (t <= 24)
-                                   __CPROVER_loop_invariant (x < 5)
-                                       __CPROVER_loop_invariant (y < 5)
-                                           __CPROVER_decreases (24 - t))
+        __loop_contract__ (
+          assigns (t; x; y; current; entire_object (lanes))
+          loop_invariant (t <= 24)
+          loop_invariant (x < 5)
+          loop_invariant (y < 5)
+          loop_decreases (24 - t)
+        )
         {
           uint64_t tmp = x;
           x = y;
@@ -122,23 +147,27 @@ kp (uint8_t state[TCT_KP1600_STATE_LEN], uint8_t rounds) __func_contract__ (
         }
       uint64_t T[5];
       for (unsigned int y = 0; y < 5; ++y)
-        __loop_contract__ (__CPROVER_assigns (
-            __CPROVER_object_whole (T), __CPROVER_object_whole (lanes),
-            y) __CPROVER_loop_invariant (y <= 5) __CPROVER_decreases (5 - y))
+        __loop_contract__ (
+          assigns (entire_object (T); entire_object (lanes); y)
+          loop_invariant (y <= 5)
+          loop_decreases (5 - y)
+        )
         {
           for (unsigned int x = 0; x < 5; ++x)
             __loop_contract__ (
-                __CPROVER_assigns (__CPROVER_object_whole (T), x)
-                    __CPROVER_loop_invariant (x <= 5)
-                        __CPROVER_decreases (5 - x))
+              assigns (entire_object (T); x)
+              loop_invariant (x <= 5)
+              loop_decreases (5 - x)
+            )
             {
               T[x] = lanes[x][y];
             }
           for (unsigned int x = 0; x < 5; ++x)
-            __loop_contract__ (__CPROVER_assigns (
-                lanes[0][y], lanes[1][y], lanes[2][y], lanes[3][y],
-                lanes[4][y], x) __CPROVER_loop_invariant (x <= 5)
-                                   __CPROVER_decreases (5 - x))
+            __loop_contract__ (
+              assigns (entire_object(lanes); x)
+              loop_invariant (x <= 5)
+              loop_decreases (5 - x)
+            )
             {
               lanes[x][y] = T[x] ^ ((~T[(x + 1) % 5]) & T[(x + 2) % 5]);
             }
@@ -146,14 +175,18 @@ kp (uint8_t state[TCT_KP1600_STATE_LEN], uint8_t rounds) __func_contract__ (
       lanes[0][0] ^= RC[round];
     }
   for (unsigned int y = 0; y < 5; ++y)
-    __loop_contract__ (__CPROVER_assigns (
-        __CPROVER_object_upto (state, TCT_KP1600_STATE_LEN),
-        y) __CPROVER_loop_invariant (y <= 5) __CPROVER_decreases (5 - y))
+    __loop_contract__ (
+      assigns (memory_slice (state, TCT_KP1600_STATE_LEN); y)
+      loop_invariant (y <= 5)
+      loop_decreases (5 - y)
+    )
     {
       for (unsigned int x = 0; x < 5; ++x)
-        __loop_contract__ (__CPROVER_assigns (
-            __CPROVER_object_upto (state, TCT_KP1600_STATE_LEN),
-            x) __CPROVER_loop_invariant (x <= 5) __CPROVER_decreases (5 - x))
+        __loop_contract__ (
+          assigns (memory_slice (state, TCT_KP1600_STATE_LEN); x)
+          loop_invariant (x <= 5)
+          loop_decreases (5 - x)
+        )
         {
           to_le64 (lanes[x][y], state + y * 40 + x * 8);
         }
@@ -163,14 +196,17 @@ kp (uint8_t state[TCT_KP1600_STATE_LEN], uint8_t rounds) __func_contract__ (
 #define TCT_SHAKE_ROUNDS 24
 
 void
-tct_shake_init (uint8_t state[TCT_KP1600_STATE_LEN]) __func_contract__ (
-    __CPROVER_requires (__CPROVER_is_fresh (state, TCT_KP1600_STATE_LEN))
-        __CPROVER_assigns (__CPROVER_object_upto (state,
-                                                  TCT_KP1600_STATE_LEN)))
+tct_shake_init (uint8_t state[TCT_KP1600_STATE_LEN])
+    __func_contract__ (
+      requires (is_fresh (state, TCT_KP1600_STATE_LEN))
+      assigns (memory_slice (state, TCT_KP1600_STATE_LEN))
+    )
 {
   for (unsigned int i = 0; i < TCT_KP1600_STATE_LEN; ++i)
-    __loop_contract__ (__CPROVER_loop_invariant (i <= TCT_KP1600_STATE_LEN)
-                           __CPROVER_decreases (TCT_KP1600_STATE_LEN - i))
+    __loop_contract__ (
+      loop_invariant (i <= TCT_KP1600_STATE_LEN)
+      loop_decreases (TCT_KP1600_STATE_LEN - i)
+    )
     {
       state[i] = 0x0;
     }
@@ -180,16 +216,17 @@ void
 tct_shake_absorb (uint8_t state[TCT_KP1600_STATE_LEN], const uint8_t *chunk,
                   const enum tct_shake_level level)
     __func_contract__ (
-        __CPROVER_requires (__CPROVER_is_fresh (state, TCT_KP1600_STATE_LEN))
-            __CPROVER_requires (__CPROVER_is_fresh (
-                chunk, (level == TCT_SHAKE128) ? 168 : 136))
-                __CPROVER_assigns (
-                    __CPROVER_object_upto (state, TCT_KP1600_STATE_LEN)))
+      requires (is_fresh (state, TCT_KP1600_STATE_LEN))
+      requires (is_fresh (chunk, (level == TCT_SHAKE128) ? 168 : 136))
+      assigns (memory_slice (state, TCT_KP1600_STATE_LEN))
+    )
 {
   const uint8_t r = (level == TCT_SHAKE128) ? 168 : 136;
   for (uint8_t i = 0; i < r; ++i)
-    __loop_contract__ (__CPROVER_loop_invariant (i <= r)
-                           __CPROVER_decreases (r - i))
+    __loop_contract__ (
+      loop_invariant (i <= r)
+      loop_decreases (r - i)
+    )
     {
       state[i] ^= chunk[i];
     }
@@ -201,24 +238,25 @@ tct_shake_squeeze_destructive (uint8_t state[TCT_KP1600_STATE_LEN],
                                uint8_t *output, const uint64_t output_len,
                                const enum tct_shake_level level)
     __func_contract__ (
-        __CPROVER_requires (output_len < 0xffffffffffffffff - 168)
-            __CPROVER_requires (__CPROVER_is_fresh (state,
-                                                    TCT_KP1600_STATE_LEN))
-                __CPROVER_assigns (
-                    __CPROVER_object_upto (state, TCT_KP1600_STATE_LEN))
-                    __CPROVER_requires (__CPROVER_is_fresh (output,
-                                                            output_len))
-                        __CPROVER_assigns (__CPROVER_object_upto (output,
-                                                                  output_len)))
+      requires (output_len < 0xffffffffffffffff - 168)
+      requires (is_fresh (state, TCT_KP1600_STATE_LEN))
+      assigns (memory_slice (state, TCT_KP1600_STATE_LEN))
+      requires (is_fresh (output, output_len))
+      assigns (memory_slice (output, output_len))
+    )
 {
   const uint8_t r = (level == TCT_SHAKE128) ? 168 : 136;
   for (uint64_t i = 0; i < output_len; i += r)
-    __loop_contract__ (__CPROVER_loop_invariant (i <= output_len + r)
-                           __CPROVER_decreases (output_len + r - i))
+    __loop_contract__ (
+      loop_invariant (i <= output_len + r)
+      loop_decreases (output_len + r - i)
+    )
     {
       for (unsigned int j = 0; j < (output_len - i) && j < r; ++j)
-        __loop_contract__ (__CPROVER_loop_invariant (
-            j <= (output_len - i) && j <= r) __CPROVER_decreases (r - j))
+        __loop_contract__ (
+          loop_invariant (j <= (output_len - i) && j <= r)
+          loop_decreases (r - j)
+        )
         {
           output[i + j] = state[j];
         }
@@ -231,12 +269,12 @@ tct_shake_full (const uint8_t *data, const uint64_t data_len,
                 const enum tct_shake_level level, uint8_t *output,
                 const uint64_t output_len)
     __func_contract__ (
-        __CPROVER_requires (output_len < 0xffffffffffffffff - 168)
-            __CPROVER_requires (data_len < 0xffffffffffffffff - 168)
-                __CPROVER_requires (__CPROVER_is_fresh (output, output_len))
-                    __CPROVER_requires (__CPROVER_is_fresh (data, data_len))
-                        __CPROVER_assigns (__CPROVER_object_upto (output,
-                                                                  output_len)))
+      requires (output_len < 0xffffffffffffffff - 168)
+      requires (data_len < 0xffffffffffffffff - 168)
+      requires (is_fresh (output, output_len))
+      requires (is_fresh (data, data_len))
+      assigns (memory_slice (output, output_len))
+    )
 {
   const uint8_t r = (level == TCT_SHAKE128) ? 168 : 136;
   uint8_t state[TCT_KP1600_STATE_LEN];
@@ -244,24 +282,26 @@ tct_shake_full (const uint8_t *data, const uint64_t data_len,
   uint8_t buf[168];
   for (uint64_t i = 0; i <= data_len; i += r)
     __loop_contract__ (
-        __CPROVER_assigns (i, __CPROVER_object_upto (buf, r),
-                           __CPROVER_object_upto (state, TCT_KP1600_STATE_LEN))
-            __CPROVER_loop_invariant (i <= data_len + r)
-                __CPROVER_decreases (data_len + r - i))
+      assigns (i; memory_slice (buf, r); memory_slice (state, TCT_KP1600_STATE_LEN))
+      loop_invariant (i <= data_len + r)
+      loop_decreases (data_len + r - i)
+    )
     {
       for (uint64_t j = 0; j < r && j < data_len - i; ++j)
         __loop_contract__ (
-            __CPROVER_assigns (j, __CPROVER_object_upto (buf, r))
-                __CPROVER_loop_invariant (j <= (data_len - i) && j <= r)
-                    __CPROVER_decreases (r - j))
+          assigns (j; memory_slice (buf, r))
+          loop_invariant (j <= (data_len - i) && j <= r)
+          loop_decreases (r - j)
+        )
         {
           buf[j] = data[i + j];
         }
       for (uint64_t j = data_len - i; j < r; ++j)
         __loop_contract__ (
-            __CPROVER_assigns (j, __CPROVER_object_upto (buf, r))
-                __CPROVER_loop_invariant (data_len - i <= j && j <= r)
-                    __CPROVER_decreases (r - j))
+          assigns (j; memory_slice (buf, r))
+          loop_invariant (data_len - i <= j && j <= r)
+          loop_decreases (r - j)
+        )
         {
           buf[j] = 0x0;
           if (j == data_len - i)
